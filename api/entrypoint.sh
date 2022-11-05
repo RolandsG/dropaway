17cac702f96daf316608e296a5c11de7dba77725
Rolands Gerhards <115147430+gerhardsr@users.noreply.github.com>
2022-11-06 00:37:56 +0200

    nginx

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-startserver()
-{
-    python /app/manage.py runserver 0.0.0.0:8000
-}
-
-for cmd in $@
+until cd /app/src/
 do
-   $cmd
+    echo "Waiting for server volume..."
 done
+
+until ./manage.py migrate
+do
+    echo "Waiting for db to be ready..."
+    sleep 2
+done
+
+./manage.py collectstatic --noinput
+
+gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
