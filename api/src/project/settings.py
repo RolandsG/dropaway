648ccfd90bd3f50997f9d6c8cdb52053e5761f1f
Rolands Gerhards <115147430+gerhardsr@users.noreply.github.com>
2022-11-05 16:22:57 +0200

    containerization added

--- api/src/project/settings.py
+++ api/src/project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in Production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # CORS_ALLOWED_ORIGINS = [
 #     'http://127.0.0.1:3000',
