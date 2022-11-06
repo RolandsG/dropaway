82eaf62c032e15afae4c24ab5e25f65bd2618830
elvijsr <erascevs@gmail.com>
2022-11-06 03:42:16 +0200

    orders page

--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -2,9 +2,10 @@ import Box from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import { useState, useEffect } from 'react'
 import coreService from '../services/core'
+import { List, ListItem, ListItemText, ListItemButton, ListItemAvatar, Avatar } from '@mui/material'
 
 const Orders = () => {
-  const text = 'Orders page'
+  const text = 'My orders:'
 
   const [orders, setOrders] = useState()
 
@@ -19,14 +20,31 @@ const Orders = () => {
   }, [])
 
   return (
-    <Box sx={{ display: 'flex', mt: 1, justifyContent: 'center' }}>
-      <Typography variant="body1" color="grey" sx={{ fontStyle: 'italic', color: '#a3a3a3' }}>
-        {text}
-      </Typography>
-      {orders &&
-        orders.map((order) => {
-          return <Box disableGutters key={order} sx={{ mt: 1 }}></Box>
-        })}
+    <Box sx={{ display: 'flex', mt: 1, justifyContent: 'center', flexDirection: 'column' }}>
+      <Typography variant="h6">{text}</Typography>
+      <List
+        disablePadding
+        sx={{
+          display: 'flex',
+          mt: 1,
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        {orders &&
+          orders.map((listing) => {
+            return (
+              <ListItem disablePadding dense key={listing.id} sx={{ mt: 1 }}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <img src={listing.photo_src} />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={listing.title} secondary={listing.category} />
+              </ListItem>
+            )
+          })}
+      </List>
     </Box>
   )
 }
